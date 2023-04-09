582b92ae623bfb3dcac895de42184ed3fbd5bcaa
MdMehedi-Hasan <mh19115@gmail.com>
2023-04-09 14:59:29 +0600

    path specified

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ const app = express()
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
 const port = 5000
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Backend active!')
+    res.send('Backend active!')
 })
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods: ["GET","POST"],
+    }
+});
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('user connected',socket.id);
 
     // It fires when a user disconnects //
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected',socket.id);
       });
   });
 
-app.listen(port, () => {
+// Generally we listen app here, but since we have required app inside server (line-5) that's why we are listening server here...
+server.listen(port, () => {
   console.log(`Backend active on port ${port}!!!`)
 })
